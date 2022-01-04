0b7b04b0ffefeb48587bda66c709e46f6ae21cb7
tech@strangersoccer.com <a.pranasakti@gmail.com>
2022-01-04 15:54:45 +0700

    basic discount calculation result testing and based on the values that is written on the coding test instruction

--- src/components/__test__/DiscountCalculator.test.tsx
+++ src/components/__test__/DiscountCalculator.test.tsx
@@ -1,19 +1,67 @@
 import DiscountCalculator from "../DiscountCalculator";
-import { render } from "@testing-library/react";
+import { render, fireEvent } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
 
-let getByTestId:any;
-let resultEl:HTMLElement;
+let getByTestId: any;
+let resultEl: HTMLElement;
+let shirtOneEl: HTMLElement;
+let shirtTwoEl: HTMLElement;
+let shirtThreeEl: HTMLElement;
+let shirtFourEl: HTMLElement;
+let shirtFiveEl: HTMLElement;
 
 beforeEach(() => {
   getByTestId = render(<DiscountCalculator />).getByTestId;
   resultEl = getByTestId("result");
+  shirtOneEl = getByTestId("shirt-1");
+  shirtTwoEl = getByTestId("shirt-2");
+  shirtThreeEl = getByTestId("shirt-3");
+  shirtFourEl = getByTestId("shirt-4");
+  shirtFiveEl = getByTestId("shirt-5");
 });
 
 test("Test initial values", () => {
   expect(resultEl.textContent).toBe("0");
 });
 
-test("Fire event: Test 2 first shirt, 2 second shirt, 2 third shirt, 1 fourth shirt, 1 fifth shirt", () => {
-  expect(resultEl.textContent).toBe("0");
+test("Fire event: Input 1 first shirt, 1 second shirt", () => {
+  fireEvent.click(shirtOneEl);
+  fireEvent.click(shirtTwoEl);
+  expect(resultEl.textContent).toBe("15.2");
+});
+
+test("Fire event: Input 1 first shirt, 1 second shirt, 1 third shirt", () => {
+  fireEvent.click(shirtOneEl);
+  fireEvent.click(shirtTwoEl);
+  fireEvent.click(shirtThreeEl);
+  expect(resultEl.textContent).toBe("21.6");
+});
+
+test("Fire event: Input 1 first shirt, 1 second shirt, 1 third shirt, 1 fourth shirt", () => {
+  fireEvent.click(shirtOneEl);
+  fireEvent.click(shirtTwoEl);
+  fireEvent.click(shirtThreeEl);
+  fireEvent.click(shirtFourEl);
+  expect(resultEl.textContent).toBe("25.6");
+});
+
+test("Fire event: Input 1 first shirt, 1 second shirt, 1 third shirt, 1 fourth shirt, 1 five shirt", () => {
+  fireEvent.click(shirtOneEl);
+  fireEvent.click(shirtTwoEl);
+  fireEvent.click(shirtThreeEl);
+  fireEvent.click(shirtFourEl);
+  fireEvent.click(shirtFiveEl);
+  expect(resultEl.textContent).toBe("30");
+});
+
+test("Fire event: Input 2 first shirt, 2 second shirt, 2 third shirt, 1 fourth shirt, 1 fifth shirt", () => {
+  fireEvent.click(shirtOneEl);
+  fireEvent.click(shirtOneEl);
+  fireEvent.click(shirtTwoEl);
+  fireEvent.click(shirtTwoEl);
+  fireEvent.click(shirtThreeEl);
+  fireEvent.click(shirtThreeEl);
+  fireEvent.click(shirtFourEl);
+  fireEvent.click(shirtFiveEl);
+  expect(resultEl.textContent).toBe("51.2");
 });
--- src/functions/calculateDiscount.ts
+++ src/functions/calculateDiscount.ts
@@ -14,14 +14,19 @@ const calculateDiscount = (cart: number[]): number => {
   let totalPrice: number = 0;
 
   let currentCart: number[] = cart;
-  while (uniqueArray<number>(currentCart).length >= 4) {
-    //Checks whether or not the cart has 4 combos
-    if (checkCombo(4, currentCart)) {
-      totalCombos.push(4);
-      let uniqueShirts: number[] = uniqueArray<number>(currentCart).slice(0, 4);
-      uniqueShirts.forEach((shirt: number) => {
-        currentCart = removeArrayItemOnce(currentCart, shirt);
-      });
+  if (currentCart.length >= 8) {//If cart lengths has more than 8 items, it will prioritize combo 4 checks.
+    while (uniqueArray<number>(currentCart).length >= 4) {
+      //Checks whether or not the cart has 4 combos
+      if (checkCombo(4, currentCart)) {
+        totalCombos.push(4);
+        let uniqueShirts: number[] = uniqueArray<number>(currentCart).slice(
+          0,
+          4
+        );
+        uniqueShirts.forEach((shirt: number) => {
+          currentCart = removeArrayItemOnce(currentCart, shirt);
+        });
+      }
     }
   }
 
@@ -30,6 +35,17 @@ const calculateDiscount = (cart: number[]): number => {
     if (checkCombo(5, currentCart)) {
       totalCombos.push(5);
       let uniqueShirts: number[] = uniqueArray<number>(currentCart).slice(0, 5);
+      uniqueShirts.forEach((shirt: number) => {
+        currentCart = removeArrayItemOnce(currentCart, shirt);
+      });
+    }
+  }
+
+  while (uniqueArray<number>(currentCart).length >= 4) {
+    //Checks whether or not the cart has 4 combos
+    if (checkCombo(4, currentCart)) {
+      totalCombos.push(4);
+      let uniqueShirts: number[] = uniqueArray<number>(currentCart).slice(0, 4);
       uniqueShirts.forEach((shirt: number) => {
         currentCart = removeArrayItemOnce(currentCart, shirt);
       });
@@ -57,21 +73,21 @@ const calculateDiscount = (cart: number[]): number => {
       });
     }
   }
-  
+
   totalCombos.forEach((combo: number) => {
-    switch(combo){
-        case 5:
-            totalPrice = totalPrice + ((price * 75) / 100) * combo;
-            break;
-        case 4:
-            totalPrice = totalPrice + ((price * 80) / 100) * combo;
-            break;
-        case 3:
-            totalPrice = totalPrice + ((price * 90) / 100) * combo;
-            break;
-        case 2:
-            totalPrice = totalPrice + ((price * 95) / 100) * combo;
-            break;
+    switch (combo) {
+      case 5:
+        totalPrice = totalPrice + ((price * 75) / 100) * combo;
+        break;
+      case 4:
+        totalPrice = totalPrice + ((price * 80) / 100) * combo;
+        break;
+      case 3:
+        totalPrice = totalPrice + ((price * 90) / 100) * combo;
+        break;
+      case 2:
+        totalPrice = totalPrice + ((price * 95) / 100) * combo;
+        break;
     }
   });
 
